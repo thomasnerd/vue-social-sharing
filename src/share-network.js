d66d699755bac70ac1dcc5367320921364e86b12
NEO-DERM\thomas.wu <thomas.wu@neoderm.com.hk>
2020-10-30 11:51:02 +0800

    refactor to not using composition api

--- src/share-network.js
+++ src/share-network.js
@@ -1,5 +1,4 @@
 import AvailableNetworks from './networks'
-import { useBrowserAgentRelated } from './browser_agent.js';
 
 let $window = typeof window !== 'undefined' ? window : null
 
@@ -95,13 +94,6 @@ export default {
       })
     }
   },
-  setup(props, { root }) {
-    const browserAgentRelated = useBrowserAgentRelated( root );
-    return {
-      //useBrowserAgentRelated
-      ...browserAgentRelated
-    }
-  },
 
   data () {
     return {
@@ -180,7 +172,41 @@ export default {
       }
 
       return this.hashtags
+    },
+    /**
+     * Add browserAgent to detect if it is IE/Edge, for disabling popupWindow use
+     */
+    browserAgent() {
+      var inBrowser = typeof window !== "undefined";
+      var browser_UA = inBrowser && window.navigator.userAgent.toLowerCase();
+      var browser_isIE = browser_UA && /msie|trident/.test(browser_UA);
+      var browser_isIE11 = browser_isIE && browser_UA.indexOf('11.0') > 0;
+      var browser_isEdge = browser_UA && browser_UA.indexOf('edge/') > 0;
+      var browser_isAndroid = browser_UA && browser_UA.indexOf('android') > 0;
+      var browser_isIOS = browser_UA && /iphone|ipad|ipod|ios/.test(browser_UA);
+      var browser_isChrome = browser_UA && /chrome\/\d+/.test(browser_UA) && !browser_isEdge;
+      var browser_isGoogleChrome = browser_isChrome && !!window.googleapis;
+      var browser_isChromeDerivate = browser_isChrome && !browser_isGoogleChrome;
+      var browser_isFirefox = browser_UA && !!browser_UA.indexOf("firefox/");
+
+      //ANOTHER:
+      // var browser_FFversion = browser_isFirefox && browser_UA.match(/firefox\/(\d+)/)[1];
+      // FF version then stored in array[1]
+      
+
+      return { 
+        isIE: browser_isIE, 
+        isIE11: browser_isIE11,
+        isEdge: browser_isEdge,
+        isAndroid: browser_isAndroid,
+        isIOS: browser_isIOS,
+        isChrome: browser_isChrome,
+        isGoogleChrome: browser_isGoogleChrome,
+        isChromeDerivate: browser_isChromeDerivate,
+        isFirefox: browser_isFirefox
+      }    
     }
+
   },
 
   render: function (createElement) {
--- src/vue-social-sharing.js
+++ src/vue-social-sharing.js
@@ -1,11 +1,8 @@
 import AvailableNetworks from './networks'
 import ShareNetwork from './share-network'
-// import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api'
 
 export default {
   install: (Vue, options) => {
-    Vue.use(VueCompositionAPI)
     Vue.component(ShareNetwork.name, ShareNetwork)
 
     Vue.prototype.$SocialSharing = {
